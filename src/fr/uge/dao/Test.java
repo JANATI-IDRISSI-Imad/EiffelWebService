d69eb850179fb59bd9876b9b950a12956110ea67
driss21 <drissnfifi41@gmail.com>
2020-11-11 19:25:21 +0100

    yaaarbiykhdem

--- src/fr/uge/dao/Test.java
+++ src/fr/uge/dao/Test.java
@@ -1,13 +1,11 @@
 package fr.uge.dao;
 
 public class Test {
-	//ZABA hamid
 	
 	public static void main(String[] args) {
 		System.out.println("nodo tqowdo");
-		//jdfipejpozkedà)kkà)
+		System.out.println("lah yrdi 3li khdem");
 	}
-	//fgsdfgsdfgdsgf
+	
 }
 
-//test
